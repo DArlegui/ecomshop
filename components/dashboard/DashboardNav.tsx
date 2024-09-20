389732b4c54f3fa3105f7e4c2765a05b34200165
darlegui <arleguidaniel@gmail.com>
2024-09-20 08:23:47 -0700

    nav bar, dashboard layouts, added shadcn

---
+++ components/dashboard/DashboardNav.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+
+const links = [
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Orders', href: '/dashboard/orders' },
+  { name: 'Products', href: '/dashboard/products' },
+  { name: 'Categories', href: '/dashboard/categories' },
+];
+
+export default function DashboardNav() {
+  return (
+    <>
+      {links.map((link) => (
+        <Link key={link.href} href={link.href}>
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+}
