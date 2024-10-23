23501f674eae0c07ca7b7d9847ee4418912eaa3a
darlegui <arleguidaniel@gmail.com>
2024-10-23 10:57:32 -0700

    Created dynamic product id route and ImageSlider.tsx

---
+++ app/(storefront)/product/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { ImageSlider } from '@/components/storefront/ImageSlider';
+import prisma from '@/lib/db';
+import { notFound } from 'next/navigation';
+
+async function getData(productId: string) {
+  const data = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+    select: {
+      price: true,
+      images: true,
+      description: true,
+      name: true,
+      id: true,
+    },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+}
+
+export default async function ProductIdRoute({ params }: { params: { id: string } }) {
+  const data = await getData(params.id);
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
+        <ImageSlider images={data.images} />
+        {/* Continue with right side, name, desc, etc.. */}
+      </div>
+    </>
+  );
+}
